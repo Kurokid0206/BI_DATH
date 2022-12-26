fb7d3fa07c84060bdbb8ce0e6d2cd93da1fdcd07
Phạm Tân Tị <kurokid0206@gmail.com>
2022-12-26 21:44:31 +0700

    feat: add package and sql

--- sql/CovidCases_NDS.sql
+++ sql/CovidCases_NDS.sql
@@ -12,5 +12,19 @@ create table PublicHealthUnit(
 	PHU_Website varchar(60),
 	PHU_Latitude float,
 	PHU_Longitude float,
-	
+	CONSTRAINT PK_PHU PRIMARY KEY(PHU_ID)
+)
+
+
+CREATE TABLE CovidCases(
+	Case_id Int Not Null Identity(1,1),
+	Age_group VarChar(20),
+	Date_id Date,
+	Gender Char(1),
+	Exposure VarChar(20), --Exposure + CaseAcquisitionInfo
+	Case_status VarChar(20), --Fatal = Deceased, Active = Not Resolve, Recover = Resolved, Not Reported (outcome = case status)
+	OutbreakRelated Bit,
+	PHU_ID Char(4),
+	CONSTRAINT PK_CovidCases PRIMARY KEY(Case_id),
+	CONSTRAINT FK_CC_PHU FOREIGN KEY(PHU_ID) REFERENCES PublicHealthUnit
 )
